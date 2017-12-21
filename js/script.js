acdc47ad9328de82f174d2c7a5060eed73cf047e
Ezell <lynellf@gmail.com>
2017-12-21 08:06:03 -0600

    inital project complete

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 // Create an array of JavaScript objects to hold the data for your quotes. Name the array quotes.
 // The quotes array should be accessible in the global scope.
 
+// Each quote object in the quotes array should have the following properties:
+    // A quote property which contains a string: the text of the quote that will be displayed on the page.
+    // A source property which contains a string identifying the creator of the quote. For example: "Mark Twain" or "Traditional Irish proverb.”
+    // An optional citation property which contains a string identifying where the quote comes from, 
+    // like a speech or publication. For example, "Famous Anonymous Jokes." If there is no known publication, do not include this property on the object.
+    // An optional year property which contains a number identifying the date of the quote. 
+    // For example, 1997. If there is no known date, then do not include this property on the object.
+
 const quotes = 
 [
     {
@@ -24,19 +32,18 @@ const quotes =
         year: '1987',
         tags: 'Motivational'
     }
-]
-
-// Each quote object in the quotes array should have the following properties:
-    // A quote property which contains a string: the text of the quote that will be displayed on the page.
-    // A source property which contains a string identifying the creator of the quote. For example: "Mark Twain" or "Traditional Irish proverb.”
-    // An optional citation property which contains a string identifying where the quote comes from, 
-    // like a speech or publication. For example, "Famous Anonymous Jokes." If there is no known publication, do not include this property on the object.
-    // An optional year property which contains a number identifying the date of the quote. 
-    // For example, 1997. If there is no known date, then do not include this property on the object.
+],
 
 // Create a function named getRandomQuote which:
     // selects a random quote object from the quotes array
     // returns the randomly selected quote object
+
+    getRandomQuote = (array) => {
+        const min = 0,
+        max = Math.floor(array.length);
+        let randomNum = Math.floor(Math.random() * (max-min) + min);
+        return array[randomNum];
+    },
 
 // Create a function named printQuote which follows these rules:
     // printQuote calls the getRandomQuote function and stores the returned quote object in a variable
@@ -51,6 +58,45 @@ const quotes =
     // printQuote doesn't add a for a missing citation or a if the year property is missing
     // printQuote displays the final HTML string to the page. 
     // You can use this JS snippet to accomplish that: document.getElementById('quote-box').innerHTML
+
+    printQuote = () => {
+        const quote = getRandomQuote(quotes),
+        quoteBox = document.querySelector('#quote-box');
+        let quoteHtml;
+
+        if(quote.citation === '') {
+            quoteHtml = 
+            `
+            <p class ="quote"> ${quote.quote} </p>
+            <p class ="source"> ${quote.source} 
+                <span class ="year">${quote.year}</span> 
+            </p>
+    
+            `;
+        } else if (quote.year === '') {
+            quoteHtml = 
+            `
+            <p class ="quote"> ${quote.quote} </p>
+            <p class ="source"> ${quote.source} 
+                <span class ="citation"> ${quote.citation} </span>  
+            </p>
+    
+            `;
+        } else {
+            quoteHtml = 
+            `
+            <p class ="quote"> ${quote.quote} </p>
+            <p class ="source"> ${quote.source} 
+                <span class ="citation"> ${quote.citation} </span> 
+                <span class ="year">${quote.year}</span> 
+            </p>
+    
+            `;
+        }
+
+        quoteBox.innerHTML = quoteHtml;
+
+    }
 
 // Add comments to your code.
 
@@ -71,4 +117,3 @@ const quotes =
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
