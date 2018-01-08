e54dc6df5d2362e3ab61e0295ea3566867b53037
Ezell L. Frazier <lynellf@gmail.com>
2018-01-08 06:52:32 -0600

    added two more quotes

--- js/script.js
+++ js/script.js
@@ -1,63 +1,75 @@
 // Each quote object in the quotes array should have the following properties:
-    // A quote property 
-    // A source property 
-    // A citation property (optional)
-    // A year property  (optional)
+// A quote property 
+// A source property 
+// A citation property (optional)
+// A year property  (optional)
 
-const quotes = 
-[
-    {
-        quote: 'After some other mistakes, I learned to go into business only with people whom I like, trust and admire',
-        source: 'Warren Buffett',
-        citation: 'Chairman\'s Letter',
-        year: '1989',
-        tags: 'Management'
-    },
-    {
-        quote: 'There are people who want to achieve--and then there are sane people',
-        source: 'Richard Koch',
-        citation: 'The 80/20 Principle: The Secret to Achieving More with Less',
-        year: '1997',
-        tags: 'Motivational'
-    },
-    {
-        quote: 'Small, smart choices + consistency + time = radical difference',
-        source: 'Darren Hardy',
-        citation: 'The Compound Effect',
-        year: '1987',
-        tags: 'Motivational'
-    }
-],
+const quotes = [{
+            quote: 'After some other mistakes, I learned to go into business only with people whom I like, trust and admire',
+            source: 'Warren Buffett',
+            citation: 'Chairman\'s Letter',
+            year: '1989',
+            tags: 'Management'
+        },
+        {
+            quote: 'There are people who want to achieve--and then there are sane people',
+            source: 'Richard Koch',
+            citation: 'The 80/20 Principle: The Secret to Achieving More with Less',
+            year: '1997',
+            tags: 'Motivational'
+        },
+        {
+            quote: 'Small, smart choices + consistency + time = radical difference',
+            source: 'Darren Hardy',
+            citation: 'The Compound Effect',
+            year: '1987',
+            tags: 'Motivational'
+        },
+        {
+            quote: 'Invention and discoveries have emanated from creative minds that have been constantly working and imagine the outcome',
+            source: 'A. P. J. Abdul Kalam',
+            citation: 'Rajya Sabha TV',
+            year: '2012',
+            tags: 'Motivational'
+        },
+        {
+            quote: 'Monotony is poverty, whether in speech or in life',
+            source: 'Dale Carnegie',
+            citation: 'Art of Public Speaking',
+            year: '1915',
+            tags: 'Motivational'
+        }
+    ],
 
-// The function named getRandomQuote which:
+    // The function named getRandomQuote which:
     // selects a random quote object from the quotes array
     // returns the randomly selected quote object
 
     getRandomQuote = (array) => {
         const min = 0,
-        max = Math.floor(array.length);
-        let randomNum = Math.floor(Math.random() * (max-min) + min);
+            max = Math.floor(array.length);
+        let randomNum = Math.floor(Math.random() * (max - min) + min);
         return array[randomNum];
     },
 
     printQuote = () => {
-         // printQuote calls the getRandomQuote function and stores the returned quote object in a variable
-         // printQuote generates three separate rgb values up to 250. 
-            // 255, 255, 255, is all white which would clash with the text.
+        // printQuote calls the getRandomQuote function and stores the returned quote object in a variable
+        // printQuote generates three separate rgb values up to 250. 
+        // 255, 255, 255, is all white which would clash with the text.
         const quote = getRandomQuote(quotes),
-        rgbaMin = 0,
-        rgbaMax = 250,
-        body = document.querySelector('body');
+            rgbaMin = 0,
+            rgbaMax = 250,
+            body = document.querySelector('body');
         quoteBox = document.querySelector('#quote-box');
         let quoteHtml,
-        randomColor1 = Math.floor(Math.random() * (rgbaMax - rgbaMin) + rgbaMin),
-        randomColor2 = Math.floor(Math.random() * (rgbaMax - rgbaMin) + rgbaMin),
-        randomColor3 = Math.floor(Math.random() * (rgbaMax - rgbaMin) + rgbaMin);
+            randomColor1 = Math.floor(Math.random() * (rgbaMax - rgbaMin) + rgbaMin),
+            randomColor2 = Math.floor(Math.random() * (rgbaMax - rgbaMin) + rgbaMin),
+            randomColor3 = Math.floor(Math.random() * (rgbaMax - rgbaMin) + rgbaMin);
 
         // printQuote doesn't add a for a missing citation or a if the year property is missing
-        if(quote.citation === '') {
-            quoteHtml = 
-            `
+        if (quote.citation === '') {
+            quoteHtml =
+                `
             <p class ="quote"> ${quote.quote} </p>
             <p class ="source"> ${quote.source} 
                 <span class ="year">${quote.year}</span> 
@@ -65,8 +77,8 @@ const quotes =
     
             `;
         } else if (quote.year === '') {
-            quoteHtml = 
-            `
+            quoteHtml =
+                `
             <p class ="quote"> ${quote.quote} </p>
             <p class ="source"> ${quote.source} 
                 <span class ="citation"> ${quote.citation} </span>  
@@ -74,8 +86,8 @@ const quotes =
     
             `;
         } else {
-            quoteHtml = 
-            `
+            quoteHtml =
+                `
             <p class ="quote"> ${quote.quote} </p>
             <p class ="source"> ${quote.source} 
                 <span class ="citation"> ${quote.citation} </span> 
